e2f3213c2d96347ad2d7f86292268222ca8dc3e7
baco16g <baco.16g@gmail.com>
2018-03-25 18:07:15 +0900

    (feat) utilsとしてrenderFiledを追加

---
+++ client/scripts/presentation/views/_core/atoms/renderSelectField.js
@@ -0,0 +1,28 @@
+/* @flow */
+import React, { Fragment } from 'react'
+
+type Props = {
+  input: Object,
+  name: string,
+  label: string,
+  meta: Object,
+  options: Array<any>
+}
+
+export default function renderSelectField({
+  input,
+  name,
+  label,
+  meta: { touched, error, warning },
+  options
+}: Props) {
+  return (
+    <Fragment>
+      <label htmlFor={name}>{label}</label>
+      <select {...input}>{options}</select>
+      {touched &&
+        ((error && <span>{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </Fragment>
+  )
+}
