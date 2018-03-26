3025d93055928490d2da16ad71e0bfa377288adf
baco16g <baco.16g@gmail.com>
2018-03-26 11:52:17 +0900

    (fix) products generator

--- __tasks/generatorProducts.js
+++ __tasks/generatorProducts.js
@@ -9,6 +9,8 @@ const PRODUCTS_PATH = '/common/products'
 const MAX_LENGTH = 15
 faker.locale = 'ja'
 
+const dbPath = path.join(PATH.SERVER, '/db/index.json')
+
 const generateRandomString = (radix, length) => {
   return Math.random()
     .toString(radix)
@@ -34,29 +36,32 @@ const saveThumb = (url, productCode, ouputPath) => {
   })
 }
 
-const list = new Array(MAX_LENGTH).fill(0).map(() => {
-  const productCode = faker.random.uuid()
-  const thumbUrl = `https://loremflickr.com/680/480?${faker.random.number()}`
-  saveThumb(thumbUrl, productCode, path.join(PATH.IMAGE_DEST, PRODUCTS_PATH))
-  return {
-    product_code: productCode,
-    volume_info: {
-      name: faker.commerce.productName(),
-      description: faker.lorem.paragraph(),
-      category: faker.lorem.slug()
-    },
-    sale_info: {
-      amount: faker.commerce.price(),
-      currency_code: 'JPY'
-    },
-    stock_info: {
-      quantity: parseInt(generateRandomString(10, 1))
+const list = () =>
+  new Array(MAX_LENGTH).fill(0).map(() => {
+    const productCode = faker.random.uuid()
+    const thumbUrl = `https://loremflickr.com/680/480?${faker.random.number()}`
+    saveThumb(thumbUrl, productCode, path.join(PATH.IMAGE_DEST, PRODUCTS_PATH))
+    return {
+      product_code: productCode,
+      volume_info: {
+        name: faker.commerce.productName(),
+        description: faker.lorem.paragraph(),
+        category: faker.lorem.slug()
+      },
+      sale_info: {
+        amount: faker.commerce.price(),
+        currency_code: 'JPY'
+      },
+      stock_info: {
+        quantity: parseInt(generateRandomString(10, 1))
+      }
     }
-  }
-})
-
-console.log(
-  JSON.stringify({
-    products: list
   })
-)
+
+fs.open(dbPath, 'r', (err, data) => {
+  const db = JSON.parse(data)
+  const json = Object.assign({}, db, {
+    products: list()
+  })
+  console.log(JSON.stringify(json))
+})
