90f830cf480c5816fbb2b53c40ee4dbe768ce5ee
baco16g <baco.16g@gmail.com>
2018-03-26 12:07:11 +0900

    (fix)

--- server/api/fetchCustomer.js
+++ server/api/fetchCustomer.js
@@ -27,13 +27,15 @@ module.exports = (req, res) => {
   const token = req.headers.authorization.split(' ')[1]
 
   jwt.verify(token, AUTH.SECRET_KEY, (err, decode) => {
-    const user = findUser(decode)
-    if (!user & err) {
+    if (!decode || err) {
       res.status(400).json({ status: -1, message: err })
-    } else if (user) {
-      res.status(200).json({ status: 0, user })
     } else {
-      res.status(200).json({ status: -2, message: 'User is not found' })
+      const user = findUser(decode)
+      if (user) {
+        res.status(200).json({ status: 0, user })
+      } else {
+        res.status(200).json({ status: -2, message: 'User is not found' })
+      }
     }
   })
 }
