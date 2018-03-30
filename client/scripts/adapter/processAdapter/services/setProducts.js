cc2c390673419c30c6a1a3d7a0d6b624919c7574
baco16g <baco.16g@gmail.com>
2018-03-30 12:05:46 +0900

    (fix) lint

--- client/scripts/adapter/processAdapter/services/setProducts.js
+++ client/scripts/adapter/processAdapter/services/setProducts.js
@@ -1,12 +1,9 @@
 /* @flow */
-import { all, fork, call, put, take, select } from 'redux-saga/effects'
+import { all, fork, call, put } from 'redux-saga/effects'
 import { map } from 'lodash'
-import { camelizeKeys, decamelizeKeys } from 'humps'
+import { camelizeKeys } from 'humps'
 import { creators as commonCreators } from '~/port/redux/common'
-import {
-  creators as productsCreators,
-  types as ProductsTypes
-} from '~/port/redux/packages/products'
+import { creators as productsCreators } from '~/port/redux/packages/products'
 import API_FUNC from '~/adapter/processAdapter/services/constants/API_FUNC'
 
 function* setProducts(): * {
